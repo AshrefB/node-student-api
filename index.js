b77cdf2b3e6bcd56f3c1f425c33052963477b775
Achref Bouadila <a_bouadila@live.fr>
2020-02-16 19:38:49 +0100

    get all students

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// DATA
+var students = [
+    {id: 1, name: 'Student 1', age: 20},
+    {id: 2, name: 'Student 2', age: 21},
+    {id: 3, name: 'Student 3', age: 22},
+]
+
 // MIDLLEWARES
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get('/api/students', (req, res) => {
+    res.status(200).json(students)
 })
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
