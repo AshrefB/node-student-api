a75ae705e05ef22402df13f34c8d88e88eedb43d
Achref Bouadila <a_bouadila@live.fr>
2020-02-16 19:20:25 +0100

    change server port dynamically

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // MIDLLEWARES
 
@@ -9,4 +10,4 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(3000, () => console.log(`Server running on http://localhost:3000`))
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
