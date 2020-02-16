cf81f873b4348288fc772e71f60036cb96e246b7
Achref Bouadila <a_bouadila@live.fr>
2020-02-16 23:47:22 +0100

    create logger and auth middlewares

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const students_router = require('./routes/students')
+const logger = require('./middlewares/logger')
+const auth = require('./middlewares/auth')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 // MIDLLEWARES
 app.use(express.json())
+app.use(logger)
+app.use(auth)
 
 // ROUTES
 app.use('/api/students', students_router)
